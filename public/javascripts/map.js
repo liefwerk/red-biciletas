6caafdf2bf288708751e290aba2fdfb5491ec142
mrclasse <natjageni@gmail.com>
2020-08-26 21:45:18 -0300

    added map

---
+++ public/javascripts/map.js
@@ -0,0 +1,5 @@
+var map = L.map('main_map').setView([-34.6023424, -58.3861497], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
